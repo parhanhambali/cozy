99e0983f240ad675c54dede4aeeae3ee8dbb6271
parhanhambali <parhanhambali13@gmail.com>
2021-04-19 17:01:23 +0700

    menambahkan splash footer

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -6,64 +6,72 @@ class SplashPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.only(
-              top: 50,
-              left: 50,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  height: 50,
-                  width: 50,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage('assets/images/logo.png'),
-                  ),
+        bottom: false,
+          child: Stack(
+            children: [
+              Align(alignment: Alignment.bottomCenter,
+                child: Image.asset('assets/images/splash_image.png')
                 ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Text(
-                'Find Cozy House\nto Stay and Happy',
-                style: blackTextStyle.copyWith(
-                  fontSize: 24,
+              Padding(
+                padding: EdgeInsets.only(
+                  top: 50,
+                  left: 50,
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                'Stop membuang banyak waktu\npada tempat yang tidak habitable',
-                style: greyTextStyle.copyWith(
-                  fontSize: 14,
-                ),
-              ),
-              SizedBox(
-              height: 40,
-              ),
-              Container(
-                width: 210,
-                height: 50,
-                child: RaisedButton(
-                  onPressed: (){},
-                  color: purpleColor,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(17),
-                  ),
-                  child: Text(
-                    'Explore Low',
-                    style: whiteTextStyle.copyWith(
-                      fontSize: 18,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/images/logo.png'),
+                      ),
                     ),
                   ),
-                ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    'Find Cozy House\nto Stay and Happy',
+                    style: blackTextStyle.copyWith(
+                      fontSize: 24,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Stop membuang banyak waktu\npada tempat yang tidak habitable',
+                    style: greyTextStyle.copyWith(
+                      fontSize: 14,
+                    ),
+                  ),
+                  SizedBox(
+                  height: 20,
+                  ),
+                  Container(
+                    width: 180,
+                    height: 40,
+                    child: RaisedButton(
+                      onPressed: (){},
+                      color: purpleColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(17),
+                      ),
+                      child: Text(
+                        'Explore Low',
+                        style: whiteTextStyle.copyWith(
+                          fontSize: 14,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
+        ),
             ],
           ),
-        ),
       ),
     );
   }
