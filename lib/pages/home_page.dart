30534cbed547e091840c7477b3ba5c976affd130
parhanhambali <parhanhambali13@gmail.com>
2021-04-25 21:01:07 +0700

    tambah BottomNavigation

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:bwa_cozy/widgets/city_card.dart';
 import 'package:bwa_cozy/models/city.dart';
 import 'package:bwa_cozy/models/tips.dart';
+import 'package:bwa_cozy/widgets/bottom_navbar_item.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -206,11 +207,48 @@ class HomePage extends StatelessWidget {
                     ),
                   ],
                 ),
+                // NOTES : BOTTOM NAVIGATION
+              ),
+              SizedBox(
+                height: 50,
               ),
             ],
           ),
         )
       ),
+      floatingActionButton: Container(
+                height: 65,
+                width: MediaQuery.of(context).size.width - (2 * edge),
+                margin: EdgeInsets.symmetric(
+                  horizontal: edge,
+                ),
+                decoration: BoxDecoration(
+                  color: Color(0xffF6F7F8),
+                  borderRadius: BorderRadius.circular(23),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    BottomNavbarItem(
+                      imageUrl: 'assets/images/icon_home.png',
+                      isActive: true,
+                    ),
+                    BottomNavbarItem(
+                      imageUrl: 'assets/images/icon_email.png',
+                      isActive: false,
+                    ),
+                    BottomNavbarItem(
+                      imageUrl: 'assets/images/icon_card.png',
+                      isActive: false,
+                    ),
+                    BottomNavbarItem(
+                      imageUrl: 'assets/images/icon_love.png',
+                      isActive: false,
+                    ),
+                  ],
+                ),
+              ),
+              floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
